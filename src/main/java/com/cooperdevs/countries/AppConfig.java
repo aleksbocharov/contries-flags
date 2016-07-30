1779d8a321504a0fd22de2c1b053bada36bc73be
aleksbocharov <sashok.san@gmail.com>
2016-07-30 18:45:32 +0200

    Initial commit

---
+++ src/main/java/com/cooperdevs/countries/AppConfig.java
@@ -0,0 +1,10 @@
+package com.cooperdevs.countries;
+
+import org.springframework.boot.SpringApplication;
+
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+
+    }
+}

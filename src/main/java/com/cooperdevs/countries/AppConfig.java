81ddc21c4724df75b00b1b9e89a7f7f7cdf04f23
aleksbocharov <sashok.san@gmail.com>
2016-07-30 18:48:22 +0200

    test connection

--- src/main/java/com/cooperdevs/countries/AppConfig.java
+++ src/main/java/com/cooperdevs/countries/AppConfig.java
@@ -5,6 +5,6 @@ import org.springframework.boot.SpringApplication;
 public class AppConfig {
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
-
+        System.out.println("test");
     }
 }
